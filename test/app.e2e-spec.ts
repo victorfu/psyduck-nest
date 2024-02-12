c1ff100b2fb96fe3cd219631e65d6f87133f2576
Victor Fu <supergothere@gmail.com>
2024-02-12 15:06:48 +0800

    fix: add global prefix /api

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -12,13 +12,11 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('/api');
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/api (GET)', () => {
+    return request(app.getHttpServer()).get('/api').expect(200).expect('pong');
   });
 });
