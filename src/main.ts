968eb7c74bc68dfe450fdff58a2b250d6d2683b6
Victor Fu <supergothere@gmail.com>
2024-02-12 18:22:36 +0800

    feat: use 8080 as default port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+  await app.listen(port);
+  console.log(`~ Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
