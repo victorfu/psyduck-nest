5ee49dbc331c3fbde22fa89ad0d7ecc9db0eb3b2
Victor Fu <supergothere@gmail.com>
2024-02-15 08:37:25 +0800

    test: fix failure of auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+
+        {
+          provide: UsersService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockImplementation((user: CreateUserDto) =>
+                Promise.resolve({ id: '1', ...user }),
+              ),
+            findAll: jest.fn().mockResolvedValue([
+              {
+                username: 'user1',
+                firstName: 'firstName #1',
+                lastName: 'lastName #1',
+              },
+              {
+                username: 'user2',
+                firstName: 'firstName #2',
+                lastName: 'lastName #2',
+              },
+            ]),
+            findOne: jest.fn().mockImplementation((id: string) =>
+              Promise.resolve({
+                username: 'user1',
+                firstName: 'firstName #1',
+                lastName: 'lastName #1',
+                id,
+              }),
+            ),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
