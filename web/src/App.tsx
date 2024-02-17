1340b6ef57b5a4f5e387bbb47f8b6b59e6ab6d49
Victor Fu <supergothere@gmail.com>
2024-02-17 21:22:50 +0800

    fix: settings url

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -229,7 +229,7 @@ function App() {
                 </li>
                 <li className="mt-auto">
                   <a
-                    href="#"
+                    href="/settings"
                     className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
                   >
                     <Cog6ToothIcon
