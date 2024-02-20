c9d9927c88364ed63d4fecdb949a560a98938222
Victor Fu <supergothere@gmail.com>
2024-02-20 18:46:46 +0800

    feat: add shadcn components

--- apps/web/src/pages/dashboard-page.tsx
+++ apps/web/src/pages/dashboard-page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 function DashboardPage() {
   return (
     <div>
       <Link to="/users">Go to Users</Link>
+      <Button>Click Me</Button>
     </div>
   );
 }
