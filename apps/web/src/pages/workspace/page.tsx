e970e4d71bafdd39712270137478e3e16c62d7bd
Victor Fu <supergothere@gmail.com>
2024-03-07 19:44:08 +0800

    fix: add empty status

--- apps/web/src/pages/workspace/page.tsx
+++ apps/web/src/pages/workspace/page.tsx
@@ -4,11 +4,51 @@ import { Menu, Transition } from "@headlessui/react";
 import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
 import { twMerge } from "tailwind-merge";
 import { format } from "date-fns";
+import { PlusIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const Empty = () => {
+  return (
+    <div className="text-center">
+      <svg
+        className="mx-auto h-12 w-12 text-gray-400"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          vectorEffect="non-scaling-stroke"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+        />
+      </svg>
+      <h3 className="mt-2 text-sm font-semibold text-gray-900">
+        No workspaces
+      </h3>
+      <p className="mt-1 text-sm text-gray-500">
+        Get started by creating a new workspace.
+      </p>
+      <div className="mt-6">
+        <Button>
+          <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+          New workspace
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 function WorkspacePage() {
   const { workspaces } = useLoaderData() as {
     workspaces: Workspace[];
   };
+
+  if (workspaces.length === 0) {
+    return <Empty />;
+  }
 
   return (
     <div>
