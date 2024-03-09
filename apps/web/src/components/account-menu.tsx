a08eb7ca438ae8602c1786c42e1c88b392d7f9e5
Victor Fu <supergothere@gmail.com>
2024-03-09 14:48:07 +0800

    fix: add username into account menu

--- apps/web/src/components/account-menu.tsx
+++ apps/web/src/components/account-menu.tsx
@@ -33,6 +33,22 @@ export function AccountMenu({ user }: { user: User }) {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+          <Menu.Item>
+            {({ active }) => {
+              return (
+                <div
+                  className={twMerge(
+                    active ? "bg-gray-50" : "",
+                    "block px-3 py-1 text-sm leading-6 text-gray-900",
+                  )}
+                >
+                  <div className="flex items-center w-full text-gray-900">
+                    {user?.username}
+                  </div>
+                </div>
+              );
+            }}
+          </Menu.Item>
           {isAdmin && (
             <Menu.Item>
               {({ active }) => {
