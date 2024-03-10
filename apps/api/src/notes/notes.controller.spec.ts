2dc888493d0638af42957ca2a9aa42f50a2c5f3b
Victor Fu <supergothere@gmail.com>
2024-03-10 23:30:45 +0800

    fix: test for note endpoints

--- apps/api/src/notes/notes.controller.spec.ts
+++ apps/api/src/notes/notes.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { NotesController } from "./notes.controller";
 import { NotesService } from "./notes.service";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Note } from "./entities/note.entity";
 
 describe("NotesController", () => {
   let controller: NotesController;
@@ -8,7 +10,19 @@ describe("NotesController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotesController],
-      providers: [NotesService],
+      providers: [
+        NotesService,
+        {
+          provide: getRepositoryToken(Note),
+          useValue: {
+            find: jest.fn().mockResolvedValue([]),
+            findOneBy: jest.fn().mockResolvedValue({}),
+            save: jest.fn().mockResolvedValue({}),
+            update: jest.fn().mockResolvedValue({}),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<NotesController>(NotesController);
--- apps/api/src/notes/notes.service.spec.ts
+++ apps/api/src/notes/notes.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { NotesService } from "./notes.service";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Note } from "./entities/note.entity";
 
 describe("NotesService", () => {
   let service: NotesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotesService],
+      providers: [
+        NotesService,
+        {
+          provide: getRepositoryToken(Note),
+          useValue: {
+            find: jest.fn().mockResolvedValue([]),
+            findOneBy: jest.fn().mockResolvedValue({}),
+            save: jest.fn().mockResolvedValue({}),
+            update: jest.fn().mockResolvedValue({}),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<NotesService>(NotesService);
