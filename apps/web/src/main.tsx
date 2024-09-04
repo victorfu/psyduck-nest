6a064c96bdd7fb25e28c706f8359efaf13f7cc4d
Victor Fu <supergothere@gmail.com>
2024-09-04 21:22:09 +0800

    fix: remove web

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,177 +1,19 @@
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
-import {
-  forgotPasswordAction,
-  loginAction,
-  loginLoader,
-  logoutAction,
-  authLoader,
-} from "./auth.ts";
-import AdminDashboardPage from "./pages/admin/dashboard/page.tsx";
-import AdminUserPage from "./pages/admin/user/page.tsx";
-import AccountPage from "./pages/account/page.tsx";
-import LoginPage from "./pages/login-page.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { authLoader } from "./auth.ts";
+
 import ErrorPage from "./pages/error-page.tsx";
-import AdminWorkspacePage from "./pages/admin/workspace/page.tsx";
-import SettingsPage from "./pages/settings/page.tsx";
-import Layout from "./components/layout.tsx";
-import {
-  adminClientsLoader,
-  adminDashboardLoader,
-  adminOrganizationsLoader,
-  adminUsersLoader,
-  workspaceListLoader,
-  adminWorkspaceLoader,
-  adminWorkspacesLoader,
-  workspaceLoader,
-  workspaceMemberLoader,
-  notesLoader,
-} from "./lib/loaders.ts";
-import AuthSuccessPage from "./pages/auth-success-page.tsx";
-import ForgotPasswordPage from "./pages/forgot-password-page.tsx";
-import { CookiesProvider } from "react-cookie";
-import AdminClientPage from "./pages/admin/client/page.tsx";
-import AdminOrganizationPage from "./pages/admin/organization/page.tsx";
-import AdminLayout from "./components/admin-layout.tsx";
-import AdminWorkspaceAccessPage from "./pages/admin/workspace-access/page.tsx";
-import WorkspaceListPage from "./pages/list/page.tsx";
-import WorkspaceLayout from "./components/workspace-layout.tsx";
-import WorkspaceClientPage from "./pages/workspace/client/page.tsx";
-import WorkspaceMemberPage from "./pages/workspace/member/page.tsx";
-import WorkspaceSettingsPage from "./pages/workspace/settings/page.tsx";
-import WorkspaceNotePage from "./pages/workspace/note/page.tsx";
-import WorkspaceNoteDetailPage from "./pages/workspace/note-detail/page.tsx";
+
+import MainPage from "./pages/main-page.tsx";
 
 const router = createBrowserRouter([
   {
     id: "root",
     path: "/",
-    element: <Layout />,
+    element: <MainPage />,
     errorElement: <ErrorPage />,
     loader: authLoader,
-    children: [
-      { index: true, element: <Navigate to="workspaces" replace /> },
-      {
-        path: "workspaces",
-        loader: workspaceListLoader,
-        element: <WorkspaceListPage />,
-      },
-      {
-        path: "notes",
-        loader: notesLoader,
-        element: <WorkspaceNotePage />,
-      },
-      {
-        path: "notes/:nid",
-        element: <WorkspaceNoteDetailPage />,
-      },
-      {
-        path: "account",
-        element: <AccountPage />,
-      },
-      {
-        path: "settings",
-        element: <SettingsPage />,
-      },
-    ],
-  },
-  {
-    id: "workspace",
-    path: "/workspaces/:wid",
-    element: <WorkspaceLayout />,
-    errorElement: <ErrorPage />,
-    loader: workspaceLoader,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="clients" replace />,
-      },
-      {
-        path: "clients",
-        element: <WorkspaceClientPage />,
-      },
-      {
-        path: "members",
-        loader: workspaceMemberLoader,
-        element: <WorkspaceMemberPage />,
-      },
-      {
-        path: "settings",
-        element: <WorkspaceSettingsPage />,
-      },
-      {
-        path: "settings/:tab",
-        element: <WorkspaceSettingsPage />,
-      },
-    ],
-  },
-  {
-    id: "adminRoot",
-    path: "/admin",
-    element: <AdminLayout />,
-    errorElement: <ErrorPage />,
-    loader: authLoader,
-    children: [
-      { index: true, element: <Navigate to="/admin/dashboard" replace /> },
-      {
-        path: "dashboard",
-        loader: adminDashboardLoader,
-        element: <AdminDashboardPage />,
-      },
-      {
-        path: "users",
-        loader: adminUsersLoader,
-        element: <AdminUserPage />,
-      },
-      {
-        path: "organizations",
-        loader: adminOrganizationsLoader,
-        element: <AdminOrganizationPage />,
-      },
-      {
-        path: "workspaces",
-        loader: adminWorkspacesLoader,
-        element: <AdminWorkspacePage />,
-      },
-      {
-        path: "workspaces/:wid",
-        loader: adminWorkspaceLoader,
-        element: <AdminWorkspaceAccessPage />,
-      },
-      {
-        path: "clients",
-        loader: adminClientsLoader,
-        element: <AdminClientPage />,
-      },
-    ],
-  },
-  {
-    path: "login",
-    action: loginAction,
-    loader: loginLoader,
-    element: <LoginPage />,
-  },
-  {
-    path: "logout",
-    action: logoutAction,
-  },
-  {
-    path: "auth/google/success",
-    element: (
-      <CookiesProvider>
-        <AuthSuccessPage />
-      </CookiesProvider>
-    ),
-  },
-  {
-    path: "forgot-password",
-    action: forgotPasswordAction,
-    element: <ForgotPasswordPage />,
   },
 ]);
 
---
+++ apps/web/src/pages/main-page.tsx
@@ -0,0 +1,13 @@
+function MainPage() {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Hello :D
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
